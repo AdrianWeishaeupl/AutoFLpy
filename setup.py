6913fab3da64f0978fffedd2ecdc145f838963cc
Weishaeupl A <aw6g15@soton.ac.uk>
2019-11-28 14:01:24 +0000

    Python 3.8 added

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     description="Automated flight log code",
     install_requires=requirements,
