cd60c7d65639c37c29a924179bb43fa95757c351
Weishaeupl A <aw6g15@soton.ac.uk>
2019-11-28 13:41:44 +0000

    Added python 3.5 to versions

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
