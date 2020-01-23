86e779283044e35bbe7d7b43207c22f5ff7c32db
Weishaeupl A <aw6g15@soton.ac.uk>
2020-01-23 09:24:24 +0000

    Release 1.0.15

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AdrianWeishaeupl/AutoFLpy.git',
-    version='1.0.14',
+    version='1.0.15',
     zip_safe=False,
 )
