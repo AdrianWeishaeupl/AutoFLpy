be6592ee2fd054de5a907c986e6d111af0a7ba2a
Weishaeupl A <aw6g15@soton.ac.uk>
2020-01-31 13:51:08 +0000

    Addition of significant_data_change_via_rms_error() code

--- autoflpy/util/flight_log_code.py
+++ autoflpy/util/flight_log_code.py
@@ -2562,11 +2562,11 @@ def backplt_map(lat, long, z_var, scale_factor=1, text_title=None, z_var_limits=
                               cmap='gnuplot', zorder=2)
         # Plots lower and upper limit data if limits are present.
         if lower_limit is not None:
-            mapplot_lower = plt.scatter(low_colour_lat_values, low_colour_long_values,
-                                        s=80, facecolors='none', edgecolors='b', marker='o', zorder=3)
+            plt.scatter(low_colour_lat_values, low_colour_long_values,
+                        s=80, facecolors='none', edgecolors='b', marker='o', zorder=3)
         if upper_limit is not None:
-            mapplot_upper = plt.scatter(high_colour_lat_values, high_colour_long_values,
-                                        s=80, facecolors='none', edgecolors='r', marker='o', zorder=4)
+            plt.scatter(high_colour_lat_values, high_colour_long_values,
+                        s=80, facecolors='none', edgecolors='r', marker='o', zorder=4)
 
     else:
         # Plots a point symbolising the mean of the data.
@@ -2677,7 +2677,62 @@ def backplt_map(lat, long, z_var, scale_factor=1, text_title=None, z_var_limits=
     return
 
 
-def take_off_graph(values_list, take_off_time, marker_list=(), arm_data=False):
+def take_off_point_finder(values_list, alt_sensitivity=0.3, groundspeed_sensitivity=0.3):
+    """Finds the take-off point from the gps flight data"""
+
+    # Imports GPS data
+    gps_index = [["altitude", "gps"], ["groundspeed", "gps"], ["time", "gps"]]
+    gps_data = []
+    for index in gps_index:
+        values_list_index = 0
+        # sets value_found to a default of False
+        # Checks to see if the 'data source' recorded in the graph list
+        # matches the 'data sources' in the list structure values_list.
+        for values_list_data in values_list:
+            # Finds data source.
+            if index[1] == values_list_data[0].lower():
+                index.append(values_list_index)
+                # Finds corresponding time source.
+                # Goes through each column searching for a match.
+                for column in values_list[values_list_index][1:]:
+                    # Checks to see if they have the same title.
+                    if column[0].lower() == index[0]:
+                        # if they do then the data is appended.
+                        gps_data.append(column)
+                        # exits for loop if data has been appended.
+                        break
+            values_list_index += 1
+
+    # find the runway altitude from the initial altitude points and uses a root mean squared error to determine the
+    # point of unacceptable altitude variation (take-off).
+    alt_data = gps_data[0][2]
+
+    # Finds the first take-off point by evaluating the developing mean and root mean squared error
+    ground_alt_data, mean_ground_alt, take_off_data_point_alt, alt_error = \
+        significant_data_change_via_rms_error(alt_data, alt_sensitivity)
+
+    take_off_time_alt = gps_data[2][2][take_off_data_point_alt]
+    print("ALT_TAKE_OFF_TIME", take_off_time_alt)
+    print("LAST ALT POINT", ground_alt_data[-1])
+    print("ALT_ERROR", alt_error)
+
+    # TODO: IS THE GROUNDSPEED NEEDED? WHAT ABOUT FAST TAXYING?
+    # Repeats the process for groundspeed
+    spd_data = gps_data[1][2]
+    ground_spd_data, mean_ground_spd, take_off_data_point_spd, spd_error = \
+        significant_data_change_via_rms_error(spd_data, groundspeed_sensitivity)
+
+    take_off_time_spd = gps_data[2][2][take_off_data_point_spd]
+    print("SPD_TAKE_OFF_TIME", take_off_time_spd)
+    print("TAKE_OFF GROUNDSPEED", ground_spd_data[-1])
+    print("SPD_ERROR", spd_error)
+
+    # TODO: Figure out a reliable way of finding the take-off point/take-off run.
+    return take_off_time_alt
+
+
+def take_off_graph(values_list, marker_list=(), take_off_time=None, arm_data=False, alt_sensitivity=0.3,
+                   groundspeed_sensitivity=0.3):
     """Plots the main variables over the take-off range of the flight on a multi-axis plot.
 
     Variables plotted:
@@ -2699,9 +2754,9 @@ def take_off_graph(values_list, take_off_time, marker_list=(), arm_data=False):
     ["vibey", "vibe"]
     ["vibez", "vibe"]
     """
-    # TODO: Finish this function:
-    #  Needs to:
-    #  Determine the take off point
+    if take_off_time is not None:
+        take_off_time = take_off_point_finder(values_list, alt_sensitivity=alt_sensitivity,
+                                              groundspeed_sensitivity=groundspeed_sensitivity)
 
     # Sets the range for all of the graphs.
     x_limits = [int(float(take_off_time) - 10), int(float(take_off_time) + 15)]
@@ -2730,12 +2785,38 @@ def take_off_graph(values_list, take_off_time, marker_list=(), arm_data=False):
                   values_list, x_limits, y_limits, marker_list, arm_data=arm_data)
 
 
-def take_off_point_finder():
-    """Finds the take-off point from the flight data"""
-    # TODO: write this function.
-    #  Needs to work for tricycle, tail dragger, engine, motor, propeller, jet..
-    #  Look into increase in GPS altitude.
-    pass
+def significant_data_change_via_rms_error(data_set, sensitivity=0.3):
+    """Finds the first point that significantly deviates from a constant value trend. This uses a developing root mean
+        squared error and compares it to a prescribes sensitivity.
+
+    Input: significant_data_change_via_rms_error(data_set, sensitivity)
+        where   data_set is a list of data to be analysed
+                sensitivity is the sensitivity with which the deviation is determined
+
+    Returns the constant_value_points list, mean(constant_value_points), the data point at which the deviation
+        occurs and the final rms_error
+     """
+    # find the runway altitude from the initial altitude points and uses a root mean squared error to determine the
+    # point of unacceptable data variation.
+    rms_error = 0
+    data_point = 0
+    constant_value_points = []
+
+    # Finds the first deviation point by evaluating the developing mean and root mean squared error
+    while rms_error < sensitivity:
+        constant_value_points.append(data_set[data_point])
+        data_point = data_point + 1
+        mean = np.mean(constant_value_points)
+        rms_error = 0
+        # Finds a root mean squared error for the current list
+        for item in constant_value_points:
+            rms_error = np.sqrt(((mean - item)**2)/data_point)
+
+    # Removes the error exceeding argument to give just points in the constant segment of data.
+    constant_value_points.pop()
+    data_point -= 1  # Last data point on the ground
+
+    return constant_value_points, np.mean(constant_value_points), data_point, rms_error
 
 
 def weather_reader(weather_data):
