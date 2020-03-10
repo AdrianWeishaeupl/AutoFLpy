1e593a3a66b450c25a682c10ac1b3fd65030e5f1
Weishaeupl A <aw6g15@soton.ac.uk>
2020-03-10 14:11:39 +0000

    Release 1.1.2

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AdrianWeishaeupl/AutoFLpy.git',
-    version='1.1.1',
+    version='1.1.2',
     zip_safe=False,
 )
