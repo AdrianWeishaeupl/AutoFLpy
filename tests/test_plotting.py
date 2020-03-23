c84ae60643d8abcddb2a7c1bd4e44e11ccbb1109
Adrian Weishaeupl <aw6g15@soton.ac.uk>
2020-03-23 10:03:23 +0000

    Added test_select_plot_data_single()

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -35,7 +35,8 @@ class TestPlotting(unittest.TestCase):
         # Running the method
         values_list_offset = plotting.manual_time_offset(plot_data, time_x_offset, number_of_flights)
 
-        # Loads "clean" values_list data
+        # Loads "clean" values_list data - plotting.manual_time_offset() seems to add the offset to all time data
+        # - no matter what variable it is in. Known to affect self.values_list, plot_data and values_list_offset
         values_list = pk.load(open(self.base_path + "test_pickled_data.pkl", "rb"))
 
         # Testing the result
@@ -51,6 +52,31 @@ class TestPlotting(unittest.TestCase):
             # Checks that the correct amount has been added to the time column
             self.assertEqual((values_list[0][1][6][7][2][data_point] + time_x_offset[0]),
                              values_list_offset[0][6][7][2][data_point])
+
+    def test_select_plot_data_single(self):
+        """Tests for select_plot_data_single()"""
+        # Assign variables
+        plot_information = [["x", "time", "gps"], ["y", "altitude", "gps"]]
+        print("TEST START")
+        # Changes the values list to only include the data
+        values_list, flight_dates_list, single_flight, number_of_flights =\
+            plotting.single_flight_detection(self.values_list)
+
+        # Runs the method
+        plot_data = plotting.select_plot_data_single(values_list, plot_information, number_of_flights)
+
+        # Checks that the called data is correct
+        for data_index in range(len(values_list[0][0][14][2])):
+            # time_data_gps = values_list[0][0][14][2]
+            self.assertEqual(values_list[0][0][14][2][data_index], plot_data[0][0][1][2][data_index])
+            # altitude_data_gps = values_list[0][0][8][2]
+            self.assertEqual(values_list[0][0][8][2][data_index], plot_data[0][1][1][2][data_index])
+
+    def test_arm_data_formatting(self):
+        pass
+
+    def test_single_flight_detection(self):
+        pass
 
 
 if __name__ == '__main__':
