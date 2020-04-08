15118ebb0ffe0ebd81db686d6eb6ed174b74413c
Adrian Weishaeupl <aw6g15@soton.ac.uk>
2020-04-08 14:59:00 +0100

    cl_cd_glide_slope_calculator updated

--- autoflpy/util/unreleased/glide_detection.py
+++ autoflpy/util/unreleased/glide_detection.py
@@ -8,7 +8,6 @@ aw6g15@soton.ac.uk 2019
 
 import scipy.constants as co
 import numpy as np
-import pandas as pd
 from autoflpy.util.analysis.environmental_data_processing import *
 
 
@@ -21,15 +20,19 @@ def glide_detection(values_list):
 
 def cl_cd_glide_slope_calculator(values_list, mass):
     """Calculates the C_L and C_D of a UAV given flight data"""
-    weather_data = weather_data_dictionary(values_list)[0][0]
+    weather_data = weather_data_dictionary(values_list)[0][0]  # TODO: [0][0] currently looks at first flight only
+    aircraft_data = aircraft_data_dictionary(values_list)[0][0]
     glide_slope_deg, v_tas_mps = glide_detection(values_list)
     weight_n = co.g * mass
     rho_sl_kgperm3 = weather_data["Density_kgperm3"]
-    q_sl = 0.5 * rho_sl_kgperm3
+    s_m2 = aircraft_data["S_m2"]
+
 
     lift_n = weight_n * np.cos(np.radians(glide_slope_deg))
     drag_n = -1. * weight_n * np.sin(np.radians(glide_slope_deg))
 
     # Converts lift and drag to non-dimensional coefficients
+    cl = 2 * lift_n/(rho_sl_kgperm3 * v_tas_mps ^ 2 * s_m2)
+    cd = 2 * drag_n / (rho_sl_kgperm3 * v_tas_mps ^ 2 * s_m2)
 
-    pass
+    return cl, cd
