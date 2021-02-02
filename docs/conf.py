b941c2357c5f4452822cb58fb7d745214ca60537
Adrian Weishaeupl <aw6g15@soton.ac.uk>
2021-02-02 12:02:23 +0000

    Copyright date updated

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'AutoFLpy'
-copyright = u"2020, Adrian Benedikt Weishaeupl"
+copyright = u"2021, Adrian Benedikt Weishaeupl"
 author = u"Adrian Benedikt Weishaeupl"
 
 # The version info for the project you're documenting, acts as replacement
